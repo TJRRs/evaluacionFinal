39c57d4a60917bdbc8643f179a7b110a74f17c07
Thiago <thiagojramos@gmail.com>
2025-03-12 23:13:44 -0300

    Primer commit - Subiendo proyecto a GitHub

---
+++ src/componentes/header/Header.jsx
@@ -0,0 +1,11 @@
+import "./header.css"
+
+function Header() {
+    return(
+        <header className="header">
+            <h1>Bienvenido a casa</h1>
+        </header>
+    )
+}
+
+export default Header;
